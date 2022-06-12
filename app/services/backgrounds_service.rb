0af4ce6e8cff18201a94775a6f20a5dd89f69ab6
Oscar Santos <oscar303data@gmail.com>
2022-06-12 13:35:52 -0600

    set up backgrounds service

---
+++ app/services/backgrounds_service.rb
@@ -0,0 +1,19 @@
+
+class BackgroundsService
+  def self.get_image_weather(query)
+  end_point = '/search/photos'
+    response = connection.get(end_point) do |faraday|
+      faraday.params['client_id'] = ENV['unsplash_api_key']
+      faraday.params['query'] = query
+
+    end
+    JSON.parse(response.body, symbolize_names: true)
+
+  end
+
+  def self.connection
+    url = 'https://api.unsplash.com'
+    Faraday.new(url: url)
+  end
+
+end
