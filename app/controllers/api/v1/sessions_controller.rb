c1c9e03e41f3b9ccae5ca073cf766733dfb8b1a7
Oscar Santos <oscar303data@gmail.com>
2022-06-16 14:14:46 -0600

    refactor forecast service

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::SessionsController < ApplicationController
     if user.present? && user.authenticate(params[:password])
       render json: Api::V1::UsersSerializer.hashed(user)
     else
-      render json: MessageSerializer.hashed_login_error
+      render json: {error: 'invalid credentials'}
     end
   end
 end
