dc81a63b8e55a3293502e86abca37c738c3639a3
Oscar Santos <oscar303data@gmail.com>
2022-06-16 14:15:14 -0600

    wrap up project sweater weather

--- spec/services/backgrounds_service_spec.rb
+++ spec/services/backgrounds_service_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe BackgroundsService do
-  it 'returns an image of a location' do
+  it 'returns an image of a location', :vcr do
     response = BackgroundsService.get_image_weather("denver,co")
   end
 end
